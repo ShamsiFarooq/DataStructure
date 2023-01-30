a39cfe52fcbbe77ae10d6141f2b45a96c98463f5
ShamsiFarooq <shamsiprof@gmail.com>
2023-01-30 15:28:17 +0530

    bubble sort

---
+++ All_Sort/bubble_sort_ array.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  List<int> arr = [64, 34, 25, 12, 22, 11, 90];
+
+  int temp;
+
+  for (int i = 0; i < arr.length - 1; i++) {
+    for (int j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  print(arr);
+}
